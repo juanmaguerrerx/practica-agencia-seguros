3b0b216d33a9586d287f3e71645495f3c6c7157a
juanmaguerrerx <jguevid1609@g.educaand.es>
2024-03-06 06:32:53 +0100

    api-doc

--- agencia-seguros-back/routes/api.php
+++ agencia-seguros-back/routes/api.php
@@ -3,8 +3,11 @@
 use App\Http\Controllers\ClientesCtrl;
 use App\Http\Controllers\PagosCtrl;
 use App\Http\Controllers\PolizasCtrl;
+use App\Http\Controllers\ProvinciasCtrl;
+use App\Http\Controllers\MunicipiosCtrl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\MultipleItemsFoundException;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +24,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * Table clientes
+ * methods: GET,POST,PATCH,DELETE
+ */
 Route::apiResource('clientes',ClientesCtrl::class);
+
+/**
+ * Table polizas
+ * methods: methods: GET,POST,PATCH,DELETE
+ */
 Route::apiResource('polizas',PolizasCtrl::class);
+
+/**
+ * Table pagos
+ * methods: GET,POST,PATCH,DELETE
+ */
 Route::apiResource('pagos',PagosCtrl::class);
+
+/**
+ * Table PROVINCIAS
+ * method: GET
+ */
+Route::apiResource('provincias',ProvinciasCtrl::class);
+
+/**
+ * Table MUNICIPIOS
+ * method:
+ */
+Route::apiResource('municipios',MunicipiosCtrl::class);
